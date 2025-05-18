1a8e2eee6ef60e55cfc3741315511873f6457a39
Gio-69s <giovanni.adadja@gmail.com>
2025-05-18 11:27:35 +0000

    Refactor washup function to enhance error handling and improve user feedback

--- adj_washup.py
+++ adj_washup.py
@@ -7,10 +7,8 @@ root.title("Washup")
 root.geometry("400x400")
 
 #Apparence de la fenetre 
-ctk.set_appearance_mode("system")  # default
 ctk.set_appearance_mode("dark")
-ctk.set_appearance_mode("light")
-
+ctk.set_default_color_theme("blue")
 #Changement du logo de la fenêtre
 root.iconbitmap("favicon.ico")
 #Fonction pour demander les noms d'utilisateur et choisit aléatoirement un utilisateur
@@ -28,6 +26,7 @@ root_entry3.pack(pady=30)
 
 def washup():
     try:
+     while True:
         #Demander les noms d'utilisateur
         u1=root_entry1.get()
         u2=root_entry2.get()
@@ -36,9 +35,13 @@ def washup():
         #Création d'une liste avec les noms d'utilisateur 
         list_user=[u1,u2,u3]
 
+        [u for u in list_user if u != ""]
+
         result=random.choice(list_user)
         root_label=ctk.CTkLabel(root, text=f"Celui fera la vaisselle est {result}")
         root_label.pack(pady=10 , padx=10)
+        if root_label:
+           break
     except:
         root_label=ctk.CTkLabel(root, text="Erreur: Veuillez entrer trois noms d'utilisateur")
         root_label.pack(pady=10 , padx=10)
