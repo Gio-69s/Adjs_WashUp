314498401321925991702a3b08ae13a0e5f02a02
Gio-69s <giovanni.adadja@gmail.com>
2025-05-18 10:49:24 +0000

    Refactor washup function to improve error handling and code structure

--- adj_washup.py
+++ adj_washup.py
@@ -23,18 +23,21 @@ root_entry3.pack(pady=30)
 
 
 def washup():
-   #Demander les noms d'utilisateur
-    u1=root_entry1.get()
-    u2=root_entry2.get()
-    u3=root_entry3.get()
+    try:
+        #Demander les noms d'utilisateur
+        u1=root_entry1.get()
+        u2=root_entry2.get()
+        u3=root_entry3.get()
 
-   #Création d'une liste avec les noms d'utilisateur 
-    list_user=[u1,u2,u3]
+        #Création d'une liste avec les noms d'utilisateur 
+        list_user=[u1,u2,u3]
 
-    result=random.choice(list_user)
-    root_label=ctk.CTkLabel(root, text=f"Celui fera la vaisselle est {result}")
-    root_label.pack(pady=10 , padx=10)
-
+        result=random.choice(list_user)
+        root_label=ctk.CTkLabel(root, text=f"Celui fera la vaisselle est {result}")
+        root_label.pack(pady=10 , padx=10)
+    except:
+        root_label=ctk.CTkLabel(root, text="Erreur: Veuillez entrer trois noms d'utilisateur")
+        root_label.pack(pady=10 , padx=10)
 
 
 #Création d'un bouton pour lancer la fonction washup
